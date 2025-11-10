acb77e690e20b62cc426d792c001308722fa3f50
Jaeseong-crypto <jaeseo@umich.edu>
2025-11-09 20:01:19 -0500

    Update Elevator.cpp

--- Elevator.cpp
+++ Elevator.cpp
@@ -16,7 +16,10 @@
 using namespace std;
 
 void Elevator::tick(int currentTime) {
-    //TODO: Implement tick
+    if (servicing) {
+        if (currentTime % TICKS_PER_ELEVATOR_MOVE == 0) {
+            if (currentFloor < targetFloor) {
+                currentFloor++;
 }
 
 void Elevator::serviceRequest(int floorNum) {
@@ -57,3 +60,4 @@ int Elevator::getCurrentFloor() const {
 int Elevator::getTargetFloor() const {
     return targetFloor;
 }
+
